36481eb9a101633e95785fe438b8540b69585a54
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-10-15 19:26:30 +1100

    add web-apps application and supporting forms

---
+++ apps/web/src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Center } from 'native-base';
+import { SignInWidget } from 'shared-views';
+const Homepage = () => {
+  return (
+    <Box>
+      <Center></Center>
+    </Box>
+  );
+};
